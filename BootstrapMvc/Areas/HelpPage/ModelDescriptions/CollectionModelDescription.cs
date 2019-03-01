6b14c2049699ce9c7a0845dd17d986bcd5d07e45
周鑫 <zx-snk@msn.com>
2019-03-01 16:10:05 +0800

    添加项目文件。

---
+++ BootstrapMvc/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BootstrapMvc.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BootstrapMvc/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BootstrapMvc.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
