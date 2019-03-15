1b8c66c1756fbf2ab2d9ea8784f2a219529f2cb3
周鑫 <zx-snk@msn.com>
2019-03-15 19:56:04 +0800

    11

--- BLL/UserInfoService.cs
+++ BLL/UserInfoService.cs
@@ -11,13 +11,6 @@ namespace BLL
 
         private IUserInfoDAL UserInfoDAL = Container.Resolve<IUserInfoDAL>();
 
-        public Base_UserInfo Login(string userName, string password)
-        {
-            string strMD5 = EncyptHelper.MD5(password);
-            var model = Dal.GetModel(s => s.User_Account == userName && s.User_Pwd == strMD5);
-            return model;
-        }
-
         public override void SetDal()
         {
             Dal = UserInfoDAL;
